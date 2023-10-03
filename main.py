3ab7fd60ec9b58e4c8e2adb3dbcd25fc789c1650
Thiago Dal Santo <55465433+thiagodalsanto@users.noreply.github.com>
2023-10-03 12:14:01 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-import cv2
-from skimage.feature import graycomatrix, graycoprops
 from components.structural_and_statistical_processing import structural_and_statistical_processing
 from components.glcm_processing import glcm_processing
 from components.process_images_for_glcm import process_images_for_glcm
