82792fa6d2d69d2e6d3c53d6f31d4a213df39af9
Thiago Dal Santo <55465433+thiagodalsanto@users.noreply.github.com>
2023-10-03 14:30:02 -0300

    Update glcm_values.py

--- components/glcm_values.py
+++ components/glcm_values.py
@@ -1,15 +1,7 @@
-import os
 import numpy as np
-import matplotlib.pyplot as plt
 from skimage.feature import graycomatrix, graycoprops
-import cv2
 
-def glcm_values(image, prefix):
-    glcm_values_folder = "RESULTADO_GLCM_VALUES"
-
-    if not os.path.exists(glcm_values_folder):
-        os.makedirs(glcm_values_folder)
-
+def glcm_values(image):
     distances = [1]
     angles = [0, np.pi/4, np.pi/2, 3*np.pi/4]
 
@@ -21,11 +13,4 @@ def glcm_values(image, prefix):
     asm = graycoprops(glcm, 'ASM')[0, 0]
     energy = graycoprops(glcm, 'energy')[0, 0]
 
-    output_file = os.path.join(glcm_values_folder, f"{prefix}_glcm_metrics.txt")
-
-    with open(output_file, 'w') as f:
-        f.write(f"Dissimilarity: {dissimilarity}\n")
-        f.write(f"Homogeneity: {homogeneity}\n")
-        f.write(f"Contrast: {contrast}\n")
-        f.write(f"ASM: {asm}\n")
-        f.write(f"Energy: {energy}\n")
+    return dissimilarity, homogeneity, contrast, asm, energy
